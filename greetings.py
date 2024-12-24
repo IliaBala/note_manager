e54a452c444b654971f61b621d2d9bd0b420d1e6
Илья <baunti23k@yandex.ru>
2024-12-24 20:41:54 +0600

    Add files via upload
    Grade 1. Этап 1: Задание 1

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Илья"
+print("Имя пользователя:", username)
+title = "Язык программирования Python"
+print("Заголовок заметки:", title)
+content = "Научиться писать чистый код, создавать что-то интересное и устроиться на работу"
+print("Описание заметки", content)
+status = "В процессе"
+print("Статус заметки:", status)
+created_data = "17-12-2024"
+print(created_data)
+issue_data = "17-10-2025"
+print(issue_data)
