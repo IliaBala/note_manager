770089693355ceec1f9cb4f9f0cd151bb90ead0f
Илья <baunti23k@yandex.ru>
2024-12-25 21:35:24 +0600

    Add files via upload
    Grade 1. Этап 1: Задание 2 Даты

---
+++ data_changer.py
@@ -0,0 +1,8 @@
+created_data = "17-12-2024"
+print(created_data)
+issue_data = "17-10-2025"
+print(issue_data)
+temp_created_data = created_data[0:5]
+temp_issue_data = issue_data[0:5]
+print(temp_created_data)
+print(temp_issue_data)
