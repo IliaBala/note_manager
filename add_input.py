c61518cb78343ec384b850d2dd7a456607873f6a
Илья <baunti23k@yandex.ru>
2024-12-25 21:47:53 +0600

    Add files via upload
    Grade 1. Этап 1: Задание 3 функции input()

---
+++ add_input.py
@@ -0,0 +1,8 @@
+username = input("Как вас зовут? ")
+print(f"Здравствуйте, {username}")
+title = input("Введите заголовок заметки: ")
+print(title)
+content = input("Введите содержание заметки: ")
+print(content)
+status = input("Введите статус: ")
+print(status)
