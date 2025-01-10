c1726119b7ace3cd51bd4f7f4efaa99524cdc87b
Илья <baunti23k@yandex.ru>
2025-01-10 22:47:09 +0600

    Update final.py

--- final.py
+++ final.py
@@ -1,8 +1,8 @@
 username = [input("Как вас зовут? ")]
 content = [input("Введите содержание заметки: ")]
 status = [input("Введите статус: ")]
-created_data = input("Введите дату создания: ")
-issue_data = input("Введите дату изменения: ")
+created_data = input("Введите дату создания (формат ДД-ММ-ГГГГ): ")
+issue_data = input("Введите дату изменения (формат ДД-ММ-ГГГГ): ")
 temp_created_data = [created_data[0:5]]
 temp_issue_data = [issue_data[0:5]]
 title_1 = [input("Введите заголовок 1: ")]
