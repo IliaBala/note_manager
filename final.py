56b8a0c0fcaed42d68c8640eca54de0203184b65
Илья <baunti23k@yandex.ru>
2025-01-10 22:37:22 +0600

    Add files via upload
    Grade 1. Этап 1: Задание 5 Финал

---
+++ final.py
@@ -0,0 +1,12 @@
+username = [input("Как вас зовут? ")]
+content = [input("Введите содержание заметки: ")]
+status = [input("Введите статус: ")]
+created_data = input("Введите дату создания: ")
+issue_data = input("Введите дату изменения: ")
+temp_created_data = [created_data[0:5]]
+temp_issue_data = [issue_data[0:5]]
+title_1 = [input("Введите заголовок 1: ")]
+title_2 = [input("Введите заголовок 2: ")]
+title = [title_1 + title_2]
+note = username + content + status + temp_created_data + temp_issue_data + title
+print(note)
