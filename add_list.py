79df312fe09b401d2d3e47fe7104e91ad34a9d12
Илья <baunti23k@yandex.ru>
2025-01-11 17:24:23 +0600

    Update add_list.py
    Добавил несколько заголовков в один список

--- add_list.py
+++ add_list.py
@@ -1,6 +1,9 @@
-title = [input("Введите заголовок заметки: ")]
-print(title)
+title_1 = [input("Введите заголовок 1: ")]
+title_2 = [input("Введите заголовок 2: ")]
+title_3 = [input("Введите заголовок 3: ")]
 content = [input("Введите содержание заметки: ")]
-print(content)
 status = [input("Введите статус: ")]
+title = title_1 + title_2 + title_3
+print(title)
+print(content)
 print(status)
