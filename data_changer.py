fff2991433c8128aa9abd349f56e6f4831a4ddef
Илья <baunti23k@yandex.ru>
2025-01-11 23:07:25 +0600

    Update data_changer.py

--- data_changer.py
+++ data_changer.py
@@ -2,7 +2,7 @@ created_data = "17-12-2024"
 print(created_data)
 issue_data = "17-10-2025"
 print(issue_data)
-temp_created_data = created_data[0:5]
-temp_issue_data = issue_data[0:5]
+temp_created_data = created_data[:5]
+temp_issue_data = issue_data[:5]
 print(temp_created_data)
 print(temp_issue_data)
