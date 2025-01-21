e6aa9f54db0e9f94cdfd6371ada3f8addc7dde8c
Илья <baunti23k@yandex.ru>
2025-01-21 23:25:26 +0600

    Create check_deadline.py
    Grade 1. Этап 2: Задание 3.
    Задание: Обработка дедлайнов.
    21/01/2025

---
+++ check_deadline.py
@@ -0,0 +1,5 @@
+note = {'username': [], 'content': [], 'status': "в процессе", 'created_data': [], 'issue_data': [], 'title': [], }
+created_data = input("Введите дату создания (формат ДД-ММ-ГГГГ): ")
+issue_data = input("Введите дату изменения (формат ДД-ММ-ГГГГ): ")
+temp_created_data = [created_data[:5]]
+temp_issue_data = [issue_data[:5]]
